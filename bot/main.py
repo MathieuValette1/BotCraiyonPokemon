d771fe8ba0f1d2a1d48a82add22928640f475b33
“tellebma” <pdmtc.bellet@gmail.com>
2022-06-28 16:13:29 +0100

    adding twitter.py able to tweet ✅

--- bot/main.py
+++ bot/main.py
@@ -1,31 +1,6 @@
-import tweepy
-import json
-from os import environ
-
-API_KEY = environ['API_KEY']
-API_SECRET_KEY = environ['API_SECRET_KEY']
-
-ACCESS_TOKEN = environ['ACCESS_TOKEN']
-ACCESS_TOKEN_SECRET = environ['ACCESS_TOKEN_SECRET']
-
-def create_tweet():
-    auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-    api = tweepy.API(auth)
-
-    tweet = 'Hello Twitter'
-    api.update_status(tweet)
-
-    return tweet
 
 
 
 
-def lambda_handler(event, context):
-
-    result = create_tweet()
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps(result)
-    }
+if __name__ == '__main__':
+    print('wow !')
