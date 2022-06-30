d3e2c45a1ae08dc0c9d074cdddbc37069d47404a
mathi <mathieu.valette1@gmail.com>
2022-06-30 13:35:56 -0400

    hashtag

--- bot/utils/prompt_generator.py
+++ bot/utils/prompt_generator.py
@@ -27,6 +27,6 @@ class Prompt_generator:
 
         tweet = f"✨ N°{pokemon_id} - {prompt}" \
                 f"\n {pokemon_name_french} - {pokemon_name_japanese}" \
-                f"\n\n #pokemon #DALLE #{pokemon_name_french}"
+                f"\n\n #Pokémon #DALLE #{pokemon_name_french}"
 
         return prompt, tweet, pokemon_name_english
